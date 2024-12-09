f154aa8d4dabcbd59add4ed6836666d520da3e5b
Ahmed <koukiahmed668@gmail.com>
2024-12-09 10:32:59 +0100

    fixing grafana

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'landing-page', pathMatch: 'full' },
+  {path: 'landing-page', component: LandingPageComponent},
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
 
 ];
 
